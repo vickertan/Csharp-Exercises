66b85461a8456fda780754ace338555ac80819c0
VickerTan <vickerpangestan@gmail.com>
2024-10-04 22:54:42 +0800

    Add first challenge in 'Operations on Array'

---
+++ Operations on Array/Program.cs
@@ -0,0 +1,20 @@
+﻿// Challenge to reverse words in sentence
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+// Create an array of words in pangram
+string[] pangramWord = pangram.Split(" ");
+
+// Create an array to store reversed pangram word
+string[] reversedPangramWord = new string[pangramWord.Length];
+
+// Iterate through each word
+for (int i = 0; i < pangramWord.Length; i++)
+{
+  char[] wordChar = pangramWord[i].ToCharArray();
+  Array.Reverse(wordChar);
+  reversedPangramWord[i] = new string(wordChar);
+}
+
+string result = String.Join(" ", reversedPangramWord);
+
+Console.WriteLine(result);
