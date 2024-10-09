7421aa7980f958af0c3c553b2448429da92e8b29
VickerTan <vickerpangestan@gmail.com>
2024-10-09 21:55:14 +0800

    Add second challenge in 'Operations on Array'

---
+++ Operations on Array/Challenge 2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Challenge to parse a string of orders, sort the orders and tag possible errors
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] order_IDs = orderStream.Split(",");
+Array.Sort(order_IDs);
+
+for (int i = 0; i < order_IDs.Length; i++)
+{
+  if (order_IDs[i].Length == 4)
+    Console.WriteLine(order_IDs[i]);
+  else
+    Console.WriteLine($"{order_IDs[i]}\t- Error");
+}
