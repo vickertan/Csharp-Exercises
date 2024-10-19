a5727bbc2fcafd187ecb53bf3182e73ceca637be
VickerTan <vickerpangestan@gmail.com>
2024-10-19 21:40:27 +0800

    Add new exercise

---
+++ Format Alphanumeric Data/Program.cs
@@ -0,0 +1,58 @@
+﻿// int invoiceNumber = 1201;
+// decimal productShares = 25.4568m;
+// decimal subtotal = 2750.00m;
+// decimal taxPercentage = .15825m;
+// decimal total = 3185.19m;
+
+// Console.WriteLine($"Invoice Number: {invoiceNumber}");
+// Console.WriteLine($"  Shares: {productShares:N3} Product");
+// Console.WriteLine($"    Sub Total: {subtotal:C}");
+// Console.WriteLine($"      Tax: {taxPercentage:P2}");
+// Console.WriteLine($"    Total Billed: {total:C}");
+
+// string input = "Pad this";
+// Console.WriteLine(input.PadLeft(12, '-'));
+// Console.WriteLine(input.PadRight(12, '-'));
+
+// string paymentId = "769C";
+// string payeeName = "Mr. Stephen Ortega";
+// string paymentAmount = "$5,000.00";
+
+// var formattedLine = paymentId.PadRight(6);
+// formattedLine += payeeName.PadRight(24);
+// formattedLine += paymentAmount.PadLeft(10);
+
+// Console.WriteLine("1234567890123456789012345678901234567890");
+// Console.WriteLine(formattedLine);
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"""
+Dear {customerName},
+As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.
+
+Currently, you own {currentShares:N} shares at a return of 12.75%.
+
+Our new product, {newProduct} offers a return of {newReturn:P2}. Given your current volume, your potential profit would be {newProfit:C}.
+
+""");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+comparisonMessage = $"""
+{currentProduct.PadRight(20)}{currentReturn:P2}     {currentProfit:C} 
+{newProduct.PadRight(20)}{newReturn:P2}     {newProfit:C} 
+""";
+
+Console.WriteLine(comparisonMessage);
