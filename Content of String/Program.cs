8375811d3d2da91baad5e6aa1e8e6b6d250e79b1
VickerTan <vickerpangestan@gmail.com>
2024-10-21 14:41:19 +0800

    Add challenge exercise

--- Content of String/Program.cs
+++ Content of String/Program.cs
@@ -71,3 +71,37 @@ while (true)
 }
 */
 
+// Challenge 1
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Extract quantity
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int openingPosition = input.IndexOf(openSpan) + openSpan.Length;
+int closingPosition = input.IndexOf(closeSpan);
+int length = closingPosition - openingPosition;
+quantity = input.Substring(openingPosition, length);
+quantity = $"Quantity: {quantity}";
+
+// Replace trademark symbol with registered trademark symbol 
+const string tradeSymbol = "&trade;";
+const string regSymbol = "&reg;";
+output = input.Replace(tradeSymbol, regSymbol);
+
+// Remove opening <div> tag
+const string openDiv = "<div>";
+int openDivPoint = output.IndexOf(openDiv);
+output = output.Remove(openDivPoint, openDiv.Length);
+
+// Remove closing <div> tag
+const string closeDiv = "</div>";
+int closeDivPoint = output.IndexOf(closeDiv);
+output = output.Remove(closeDivPoint, closeDiv.Length);
+output = $"Output: {output}";
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
