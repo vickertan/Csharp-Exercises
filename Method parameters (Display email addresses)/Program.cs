422be829df8ac8132bce2f9c91346d7b8e827175
VickerTan <vickerpangestan@gmail.com>
2024-11-26 23:46:18 +0800

    Add challenge exercise

---
+++ Method parameters (Display email addresses)/Program.cs
@@ -0,0 +1,33 @@
+﻿string[,] corporate =
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external =
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+  // display internal email addresses
+  string username = corporate[i, 0][..2].ToLower() + corporate[i, 1].ToLower();
+  DisplayEmailAddress(username);
+}
+
+for (int i = 0; i < external.GetLength(0); i++)
+{
+  // display external email addresses
+  string username = external[i, 0][..2].ToLower() + external[i, 1].ToLower();
+  DisplayEmailAddress(username, externalDomain);
+}
+
+void DisplayEmailAddress(string name, string domain = "contoso.com")
+{
+  Console.WriteLine($"{name}@{domain}");
+}
